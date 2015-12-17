9cac53bdfdd03c83b5160f730d65a8081a313792
Jackson Lucas <jackson7br@gmail.com>
2015-12-17 14:02:28 -0400

    working on api requests

--- js/Components/apiHandler.js
+++ js/Components/apiHandler.js
@@ -1,8 +1,21 @@
-export default class apiHandler {
-  getCalls(start, end) {
-    // start.time
-    // start.date
-    // end.time
-    // end.date
-  }
-}
+import $ from 'jquery';
+
+export default class ApiHandler {
+  getEmergencyCalls (start, end) {
+    $.ajax({
+      url: `http://127.0.0.1:3000/getCalls/${start.date}/${start.time}/${end.date}/${end.time}/`,
+      data: {
+        format: 'json'
+      },
+      error: function(error) {
+        console.log("ERROR!!!");
+      },
+      dataType: 'jsonp',
+      success: function(data) {
+        console.log("IT WORKED!!!");
+        console.log(data);
+      },
+      type: 'GET'
+    });
+  };
+};
--- js/app.jsx
+++ js/app.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './UiComponents/component.jsx';
+import ApiHandler from './Components/apiHandler.js';
 
 ReactDOM.render(<Hello/>, document.getElementById('app'));
+
+
+var apiHandler = new ApiHandler();
+
+var start = {
+  time: '',
+  date: ''
+};
+
+var end = {
+  time: '',
+  date: ''
+};
+
+apiHandler.getEmergencyCalls(start, end);
